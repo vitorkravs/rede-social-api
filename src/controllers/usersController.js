c10fc0911f297c6ae9fa1b3887dd0695c3255634
vitor kravszenko <vitorkravs3@gmail.com>
2024-04-04 19:05:16 -0300

    feat: criando controle de registro de usuário

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,13 +1,14 @@
 import { connection } from "../db/db.js";
 
 import bcrypt from "bcrypt";
+import { v4 } from "uuid";
 
 export const LoginUser = async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
     return res
-      .status(404)
+      .status(400)
       .json({ msg: "Os campos de email e senha são obrigatórios" });
   }
 
@@ -35,5 +36,45 @@ export const LoginUser = async (req, res) => {
 };
 
 export const RegisterUser = (req, res) => {
-  return res.status(200).json({ msg: "funcionando" });
+  const { email, password, confirmPassword } = req.body;
+
+  if (!email || !password || !confirmPassword) {
+    return res.status(400).send("Todos os campos precisam ser preenchidos");
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(400).send("Senhas não são iguais");
+  }
+
+  const checkEmailQuery = "SELECT * FROM users WHERE email = ?";
+
+  connection.query(checkEmailQuery, [email], async (error, results) => {
+    if (error) {
+      return res.status(500).send("Erro no servidor");
+    }
+
+    if (results.length > 0) {
+      return res.status(409).json({ msg: "Esse email já está em uso" });
+    } else {
+      const hashPassword = await bcrypt.hash(password, 10);
+
+      const id = v4();
+
+      const insertUserToDBQuery =
+        "INSERT INTO users (id, email, password) VALUES (?, ?, ?)";
+
+      connection.query(
+        insertUserToDBQuery,
+        [id, email, hashPassword],
+        async (error, results) => {
+          if (error) {
+            return res
+              .status(500)
+              .json({ msg: "Erro ao registrar o usuário.", error });
+          }
+          res.status(201).json({ msg: "Usuário registrado com sucesso." });
+        }
+      );
+    }
+  });
 };
